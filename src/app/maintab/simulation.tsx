942eeccc2760df5f002f94776d059c3aa1271271
Gaurav Jain <gauravjain.0184@gmail.com>
2026-06-05 12:09:46 +0530

    init

---
+++ src/app/maintab/simulation.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const simulation = () => {
+  return (
+    <View>
+      <Text>simulation</Text>
+    </View>
+  )
+}
+
+export default simulation
+
+const styles = StyleSheet.create({})
